981722b8b2d72bc0f8c9428b3679afab1011f51f
MedveMarci <martonmende@gmail.com>
2026-02-06 12:54:50 +0100

    Fixed Announcement can't be disabled

--- SerpentsHand/ShWave/Patches/WaveAnnouncementPatch.cs
+++ SerpentsHand/ShWave/Patches/WaveAnnouncementPatch.cs
@@ -12,13 +12,10 @@ public static class WaveAnnouncementPatch
 {
     public static bool Prefix(WaveAnnouncementBase __instance, List<ReferenceHub> spawnedPlayers, IAnnouncedWave wave)
     {
-        if (wave is not SerpentsHandWave) return true;
+        if (wave is not SerpentsHandWave || string.IsNullOrEmpty(SerpentsHand.Singleton.Config?.ShWaveAnnouncement)) return true;
         var stringBuilder = StringBuilderPool.Shared.Rent();
         __instance.CreateAnnouncement(stringBuilder, spawnedPlayers, out _);
-        var customSubtitle = string.IsNullOrEmpty(SerpentsHand.Singleton.Config?.ShWaveSubtitle)
-            ? "<pos=-0.8%,10em> <color=#FF96DE>C.A.S.S.I.E : </color></pos>Security Alert. Serpents Hand activity detected. All security personnel must proceed with emergency protocols."
-            : SerpentsHand.Singleton.Config.ShWaveSubtitle;
-        var payload = new CassieTtsPayload(StringBuilderPool.Shared.ToStringReturn(stringBuilder), customSubtitle);
+        var payload = new CassieTtsPayload(StringBuilderPool.Shared.ToStringReturn(stringBuilder), SerpentsHand.Singleton.Config?.ShWaveSubtitle);
         new CassieWaveAnnouncement(wave, payload).AddToQueue();
         return false;
     }
